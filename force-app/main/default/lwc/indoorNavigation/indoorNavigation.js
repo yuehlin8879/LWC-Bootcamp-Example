ff2db5db7bc871d87aaf03ebdfb1e9d043111539
Kevin Lin <kevilin@deloitte.ca>
2022-05-31 22:49:14 -0400

    minor update

--- force-app/main/default/lwc/indoorNavigation/indoorNavigation.js
+++ force-app/main/default/lwc/indoorNavigation/indoorNavigation.js
@@ -1,16 +1,16 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
 // import static resources
-import NAVI_IMAGES from "@salesforce/resourceUrl/Navi_Images";
+import NAVI_IMAGES from '@salesforce/resourceUrl/Navi_Images';
 
 // import custom labels
-import PRINTBTN from "@salesforce/label/c.Print_Btn_Label";
-import CHANGEVIEWBTN from "@salesforce/label/c.Change_View_Btn_Label";
+import PRINTBTN from '@salesforce/label/c.Print_Btn_Label';
+import CHANGEVIEWBTN from '@salesforce/label/c.Change_View_Btn_Label';
 
 // views available for this application
 const VIEWMODE = {
-    NAV: "nav",
-    PRINTABLE: "printable",
+    NAV: 'nav',
+    PRINTABLE: 'printable',
 };
 
 export default class IndoorNavigation extends LightningElement {
@@ -18,7 +18,7 @@ export default class IndoorNavigation extends LightningElement {
     view = VIEWMODE.NAV;
 
     // stores the custom label values
-    labels = {
+    label = {
         PRINTBTN,
         CHANGEVIEWBTN,
     };
@@ -36,19 +36,19 @@ export default class IndoorNavigation extends LightningElement {
     // data
     directions = [
         {
-            title: "Waiting Room",
-            description: "Begin from the waiting area, head staight to the back of the room.",
-            image: NAVI_IMAGES + "/waiting_room.jpg",
+            title: 'Waiting Room',
+            description: 'Begin from the waiting area, head staight to the back of the room.',
+            image: NAVI_IMAGES + '/waiting_room.jpg',
         },
         {
-            title: "Hallway",
-            description: "Head straight towards the end of the hallway.",
-            image: NAVI_IMAGES + "/hallway.jpg",
+            title: 'Hallway',
+            description: 'Head straight towards the end of the hallway.',
+            image: NAVI_IMAGES + '/hallway.jpg',
         },
         {
-            title: "Ultrasound",
-            description: "Make a right to arrive at Ultrasound.",
-            image: NAVI_IMAGES + "/ultrasound.jpg",
+            title: 'Ultrasound',
+            description: 'Make a right to arrive at Ultrasound.',
+            image: NAVI_IMAGES + '/ultrasound.jpg',
         },
     ];
 
@@ -59,12 +59,9 @@ export default class IndoorNavigation extends LightningElement {
         // for(let i=0; i < this.directions.length; i++){
         //    this.directions[i].title = this.directions[i].title.toUpperCase();
         // }
-        this.directions = this.directions.map((direction) => {
-            return {
-                title: direction.title.toUpperCase(),
-                description: direction.description,
-                image: direction.image,
-            };
+
+        this.directions.forEach((direction) => {
+            direction.title = direction.title.toUpperCase();
         });
     }
 
@@ -72,10 +69,10 @@ export default class IndoorNavigation extends LightningElement {
     toggleView() {
         if (this.view === VIEWMODE.NAV) {
             this.view = VIEWMODE.PRINTABLE;
-            console.log("display printable view");
+            console.log('display printable view');
         } else {
             this.view = VIEWMODE.NAV;
-            console.log("display nav view");
+            console.log('display nav view');
         }
     }
 
